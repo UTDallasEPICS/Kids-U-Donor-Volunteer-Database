be045556dac7e35e42da66e9c4ec23211399e421
amilahaque <amilahaque@gmail.com>
2024-04-04 16:02:53 -0500

    Adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import GrantsPage from './pages/GrantsPage'; // Import the GrantsPage component
+import './App.css'; // This could contain global styles
+
+const App = () => {
+  return (
+    <div className="App">
+      <GrantsPage />
+    </div>
+  );
+};
+
+export default App;
