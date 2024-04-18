361276edbff875fcf5d7b3832385bbbb6934197a
Kim Bui <ktb220000@utdallas.edu>
2024-04-18 15:43:42 -0500

    updated prisma schema and clean migrations again

---
+++ prisma/migrations/20240418204319_updated_migration/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Years` on the `Grant` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Grant" DROP COLUMN "Years";
