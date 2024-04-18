198b259f9a0305209b10bad554de1d61ce6ce9aa
Ben Carcasi <ben.carcasi@gmail.com>
2024-04-17 19:16:13 -0500

    Some Api testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter, Route, and Routes
+import ApiTestPage from './pages/ApiTestPage'; // Import the GrantsPage component
 import GrantsPage from './pages/GrantsPage'; // Import the GrantsPage component
 import GrantDetailPage from './pages/GrantDetailPage'; // Import the GrantDetailPage component
 
@@ -11,6 +12,7 @@ const App = () => {
       <div className="App">
         <Routes> {/* Use the Routes component */}
           <Route path="/" element={<GrantsPage />} /> {/* Define the route for the GrantsPage */}
+          <Route path="/text" element={<ApiTestPage/>} /> {/* Define the route for a testing page */}
           <Route path="/grant/:id" element={<GrantDetailPage />} /> {/* Define the route for the GrantDetailPage */}
         </Routes>
       </div>
--- frontend/src/api/organizations/create.js
+++ frontend/src/api/organizations/create.js
@@ -1,3 +1,4 @@
+// pages/api/organizations/create.js
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
