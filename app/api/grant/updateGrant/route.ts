a3f9ad7a0a5ac6047d6fa166d9cde5527a4bde1f
Ben Carcasi <ben.carcasi@gmail.com>
2024-04-25 15:52:56 -0500

    Data save changes

--- app/api/grant/updateGrant/route.ts
+++ app/api/grant/updateGrant/route.ts
@@ -17,26 +17,61 @@ export async function PUT(Request: Request): Promise<Response> {
     const updatedData = requestBody.updatedData; 
     console.log(updatedData);
 
+    let GrantDueDate = updatedData.GrantDueDate;
+    GrantDueDate = new Date(GrantDueDate);
+    if(GrantDueDate.toString() == "Invalid Date"){
+      GrantDueDate = new Date("1970-01-01");
+    }
+    updatedData.GrantDueDate = GrantDueDate;
+
+    let AskDate = updatedData.AskDate;
+    AskDate = new Date(AskDate);
+    if(AskDate.toString() == "Invalid Date"){
+      AskDate = new Date("1970-01-01");
+    }
+    updatedData.AskDate = AskDate;
+
+    let AwardDate = updatedData.AwardDate;
+    AwardDate = new Date(AwardDate);
+    if(AwardDate.toString() == "Invalid Date"){
+      AwardDate = new Date("1970-01-01");
+    }
+    updatedData.AwardDate = AwardDate;
+
+    let DateToReapplyForGrant = updatedData.DateToReapplyForGrant;
+    DateToReapplyForGrant = new Date(DateToReapplyForGrant);
+    if(DateToReapplyForGrant.toString() == "Invalid Date"){
+      DateToReapplyForGrant= new Date("1970-01-01");
+    }
+    updatedData.DateToReapplyForGrant = DateToReapplyForGrant;
+
     let WaitingPeriodToReapply = updatedData.WaitingPeriodToReapply;
     WaitingPeriodToReapply = parseInt(WaitingPeriodToReapply);
     if(isNaN(WaitingPeriodToReapply)){
       WaitingPeriodToReapply = 0;
     }
-    updatedData.WaitingPeriodToReapply = WaitingPeriodToReapply
-    
+    updatedData.WaitingPeriodToReapply = WaitingPeriodToReapply;
+
     let AskAmount = updatedData.AskAmount;
     AskAmount = parseFloat(AskAmount);
     if(isNaN(AskAmount)){
       AskAmount = 0.0;
     }
-    updatedData.AskAmount = AskAmount
+    updatedData.AskAmount = AskAmount;
 
     let AmountAwarded = updatedData.AmountAwarded;
     AskAmount = parseFloat(AmountAwarded);
     if(isNaN(AmountAwarded)){
       AmountAwarded = 0.0;
     }
-    updatedData.AmountAwarded = AskAmount
+    updatedData.AmountAwarded = AskAmount;
+
+    let EndOfGrantReportDueDate = updatedData.EndOfGrantReportDueDate;
+    EndOfGrantReportDueDate = new Date(EndOfGrantReportDueDate);
+    if(EndOfGrantReportDueDate.toString() == "Invalid Date"){
+      EndOfGrantReportDueDate= new Date("1970-01-01");
+    }
+    updatedData.EndOfGrantReportDueDate = EndOfGrantReportDueDate;
 
 
 
