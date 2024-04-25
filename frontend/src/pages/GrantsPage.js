e86a5f0dddca96a45158abc9d3ec0dae5383f343
amilahaque <amilahaque@gmail.com>
2024-04-25 15:46:01 -0500

    Some saves work

--- frontend/src/pages/GrantsPage.js
+++ frontend/src/pages/GrantsPage.js
@@ -74,45 +74,40 @@ const SearchBar = () => (
 //This table dynamically creates rows with each grant
 const GrantsTable = ({ grants }) => (
   <div className="grants-table-container">
-    <table className="grants-table">
-      <thead>
-        <tr>
-          <th>Organization</th>
-          <th>Grant Name</th>
-          <th>Grant Status</th>
-          <th>Requested Amount</th>
-          <th>Awarded Amount</th>
-          <th>Restriction</th>
-          <th>Submission Date</th>
-          <th>Due Date</th>
-          <th>Award Date</th>
-          <th>Linked Donations</th>
-          <th>Outstanding Fund Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-       {grants.map((grant) => (
-          <tr key={grant.GrantID}>
-            <td>{/* Render organization name here */}</td>
-            <td>
-              {/* Make the Grant Name clickable with dynamic URL based on grant ID*/}
-              <Link to={`/grant/${grant.GrantID}`}>{grant.GrantName}</Link>
-            </td>
-            <td>{/* Render grant status here */}</td>
-            <td>{/* Render requested amount here */}</td>
-            <td>{/* Render awarded amount here */}</td>
-            <td>{/* Render restriction here */}</td>
-            <td>{/* Render submission date here */}</td>
-            <td>{/* Render due date here */}</td>
-            <td>{/* Render award date here */}</td>
-            <td>{/* Render linked donations here */}</td>
-            <td>{/* Render outstanding fund amount here */}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+  <table className="grants-table">
+  <thead>
+  <tr>
+  <th>Organization</th>
+  <th>Grant Name</th>
+  <th>Grant Status</th>
+  <th>Requested Amount</th>
+  <th>Awarded Amount</th>
+  <th>Restriction</th>
+  <th>Report Due Date</th>
+  <th>Due Date</th>
+  <th>Award Date</th>
+  </tr>
+  </thead>
+  <tbody>
+         {grants.map((grant) => (
+  <tr key={grant.GrantID}>
+  <td>{grant.Representative ? grant.Representative[0] : "N/A"}</td>
+  <td>
+  <Link to={`/grant/${grant.GrantID}`}>{grant.GrantName}</Link>
+  </td>
+  <td>{grant.AwardStatus || "Pending"}</td>
+  <td>${grant.AskAmount ? grant.AskAmount.toFixed(2) : "0.00"}</td>
+  <td>${grant.AmountAwarded ? grant.AwardedAmount.toFixed(2) : "0.00"}</td>
+  <td>{grant.FundingRestrictions || "None"}</td>
+  <td>{grant.EndOfGrantReportDueDate || "N/A"}</td>
+  <td>{grant.DueDate || "N/A"}</td>
+  <td>{grant.AwardDate || "Not Awarded Yet"}</td>
+  </tr>
+          ))}
+  </tbody>
+  </table>
   </div>
-);
+  );
 
 
 export default GrantsPage;
