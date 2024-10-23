490f086161ce6f918dba19c60553afb1eab61709
Tagan Ramsey <107708674+tbramsey@users.noreply.github.com>
2024-10-23 15:57:46 -0500

    Revert "Tagan"

--- app/api/donations/route.ts
+++ app/api/donations/route.ts
@@ -1,36 +1,8 @@
-import { PrismaClient } from '@prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
-
-// Route handlers are being used (Newer), not API Routes, so we have to use NextRequest/NextResponse
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-// Create
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    //console.log(body);
-    
-    return NextResponse.json(
-      {
-        message: 'POST REQUEST',
-        receivedData: body, 
-      },
-      { status: 200 } 
-    );
-  } catch (error) {
-    console.error('POST ERROR:', error);
-  }
-}
-
-
-// Read
 export async function GET() {
-  const data = await prisma.donation.findMany();
-
-  return NextResponse.json(
-    { message: 'GET REQUEST', data: data },
-    { status: 200 }
-  );
+  const data = 1;
 }
-
