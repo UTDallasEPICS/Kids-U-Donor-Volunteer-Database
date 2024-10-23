97669fab5cb05ba554044ee10b56af72456882c0
Tejas Ramanujam <101826696+TejasRamanujam@users.noreply.github.com>
2024-10-23 16:30:17 -0500

    foundation

--- app/Volunteers/Checkinout/page.tsx
+++ app/Volunteers/Checkinout/page.tsx
@@ -1,7 +1,27 @@
+
+
+const Breadcrumb = () => (
+    <div className="mb-5 text-sm text-gray-600 flex items-center space-x-2">
+      <span className="hover:text-blue-500 cursor-pointer transition-colors duration-200">
+        Home
+      </span>
+      <span className="text-gray-400">/</span>
+      <span className="font-semibold text-gray-700">Checkin : Checkout</span>
+    </div>
+  );
+
 export default function Checkinout() {
     return (
+        
       <div>
-        <h1 className="text-xl">Check-In/Out Page</h1>
+        <Breadcrumb />
+        <h1 className="text-2xl font-semibold text-gray-800">Check-In/Out</h1>
+        <button className="bg-blue-500 text-white rounded-sm p-1">
+        Check-In
+      </button>
+      <button className="bg-blue-500 text-white rounded-sm p-1">
+        Check-Out
+      </button>
       </div>
     );
   }
