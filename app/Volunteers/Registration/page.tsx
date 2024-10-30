60684745535396af0a13a77c7a8cc8e3fdbb7903
Ashvita <118849231+ashvitagirish@users.noreply.github.com>
2024-10-30 15:54:52 -0500

    reg_prog

--- app/Volunteers/Registration/page.tsx
+++ app/Volunteers/Registration/page.tsx
@@ -1,8 +1,43 @@
+"use client";
+import React, { useState } from "react";
+import Link from "@/node_modules/next/link";
+import { FormControl, TextField } from "@/node_modules/@mui/material/index";
 export default function Registration() {
-    return (
-      <div>
-        <h1 className="text-xl">Registration Page</h1>
-      </div>
-    );
-  }
+
   
+
+    const Breadcrumb = () => (
+        <div className="mb-5 text-sm text-gray-600 flex items-center space-x-2">
+          <span className="hover:text-blue-500 cursor-pointer transition-colors duration-200">
+            Home
+          </span>
+          <span className="text-gray-400">/</span>
+          <span className="font-semibold text-gray-700">Registration</span>
+        </div>
+      );
+
+      const Header = () => (
+        <div className="flex justify-between items-center mb-5">
+          <h1 className="text-2xl font-bold text-gray-800">Events</h1>
+          <div className="px-4 py-2 ml-2">
+            <Link href="/Volunteers/Registration/New_event">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-200">
+                Add A New Event
+            </button>
+          </Link>
+          </div>
+        </div>
+      );
+
+
+    return (
+      <div className={"flex font-sans"}>
+        <div className="flex-grow p-5">
+        <Breadcrumb />
+        <Header />
+
+        </div>
+      </div>
+
+    );
+ }
