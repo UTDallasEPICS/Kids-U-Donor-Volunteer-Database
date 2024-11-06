aeef7c00b5c4c443ce22b4e9ae9046c18f33e0b7
Kim Bui <ktb220000@utdallas.edu>
2024-11-06 10:04:08 -0600

    set up grantor list

---
+++ app/Grants/Grantor/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import type { Grant } from "@/prisma";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+export default function GrantsPage() {
+    /*const [grantsData, setGrantsData] = useState<Grant[]>([]); // State to store grants data
+  
+    useEffect(() => {
+      fetchGrantsData();
+    }, []);
+  
+    const fetchGrantsData = async () => {
+      try {
+        const response = await fetch("/api/grants/");
+        const result = await response.json();
+        setGrantsData(result.data);
+      } catch (error) {
+        console.error("Error fetching grants:", error);
+      }
+    };*/
+
+    return (
+        <Box>
+            <Box>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link underline="hover" color="inherit" href="/">
+                        Home
+                    </Link>
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                    >
+                        Grants
+                    </Link>
+                    <Typography sx={{ color: 'text.primary' }}>Grantor List</Typography>
+                </Breadcrumbs>
+            </Box>
+            <Box>
+                <Box
+                    component="form"
+                    sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField id="outlined-basic" label="Search" variant="outlined" size="small" />
+                </Box>
+            </Box>
+            <Box>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ fontWeight: 'bold' }}>Name</TableCell>
+                                <TableCell sx={{ fontWeight: 'bold' }}>Representative</TableCell>
+                                <TableCell sx={{ fontWeight: 'bold' }}>Type</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        {/*<TableBody>
+                            {grantsData.map((grant) => (
+                                <TableRow
+                                    key={grant.GrantID}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell>{grant.Representative
+                                        ? grant.Representative[0].FirstName
+                                        : "N/A"}
+                                    </TableCell>
+                                    <TableCell>{<Link href={`/Grants/Detail/${grant.GrantID}`}>
+                                        {grant.GrantName}
+                                    </Link>}</TableCell>
+                                    <TableCell>{grant.AwardStatus}</TableCell>
+                                    <TableCell>{grant.AskAmount}</TableCell>
+                                    <TableCell>{grant.AmountAwarded}</TableCell>
+                                    <TableCell>{grant.GrantDueDate
+                                        ? new Date(grant.GrantDueDate).toLocaleDateString()
+                                        : "N/A"}
+                                    </TableCell>
+                                    <TableCell>{grant.EndOfGrantReportDueDate
+                                        ? new Date(
+                                            grant.EndOfGrantReportDueDate
+                                        ).toLocaleDateString()
+                                        : "N/A"}
+                                    </TableCell>
+                                    <TableCell>{grant.FundingRestrictions}</TableCell>
+                                    <TableCell>{grant.AwardDate
+                                        ? new Date(grant.AwardDate).toLocaleDateString()
+                                        : "Not Awarded Yet"}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        */}
+                    </Table>
+                </TableContainer>
+            </Box>
+        </Box >
+    );
+}
