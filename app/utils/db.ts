a143c8e13d53f269e55b662a987c605a55233701
AkshayaKomaragiri <akshaya.komaragiri@gmail.com>
2025-03-10 12:53:51 -0500

    Grantors - POST and DELETE

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,8 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
+const prismaSoftDelete = new PrismaClient().$extends({
+  model: {
+    $allModels: {
+      async delete<M, A>(this: M, where: Prisma.Args<M, "delete">): Promise<Prisma.Result<M, A, "update">> {
+        const context = Prisma.getExtensionContext(this);
+
+        return (context as any).update({
+          ...where,
+          data: {
+            deletedAt: new Date(),
+            status: false,
+          },
+        });
+      },
+    },
+  },
+});
+export { prismaSoftDelete };
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
