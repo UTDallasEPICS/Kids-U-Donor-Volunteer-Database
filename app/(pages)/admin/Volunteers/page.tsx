94b8ec6315d977a70b41464c0c12b69ca2ecb2bb
Swarna Sre <swarnasre@Swarnas-MacBook-Air-2.local>
2025-04-14 17:02:14 -0500

    made an admin folder

--- app/(pages)/admin/Volunteers/page.tsx
+++ app/(pages)/admin/Volunteers/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import type { Volunteer } from "@/prisma";
+import type { Volunteer } from "../../../../prisma";
 import {
   Box,
   TextField,
